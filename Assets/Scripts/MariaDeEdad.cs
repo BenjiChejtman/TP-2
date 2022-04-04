3645d37d8f268feadeecae3bf1ea47808f5440da
BenjiChejtman <46895375@est.ort.edu.ar>
2022-04-04 12:10:11 -0300

    "Es mayor de edad" Uploaded

---
+++ Assets/Scripts/MariaDeEdad.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MariaDeEdad : MonoBehaviour
+{
+    public int EdadUsuario;
+    public Text MiTexto;
+
+    void Start()
+    {
+        if (EdadUsuario >= 18)
+        {
+            MiTexto.text = "El usuario es mayor de edad.";
+            Debug.Log("El usuario es mayor de edad.");
+        }
+        else
+        {
+            MiTexto.text = "El usuario es menor de edad.";
+            Debug.Log("El usuario es menor de edad.");  
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
