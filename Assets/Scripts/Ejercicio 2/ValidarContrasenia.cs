1c2b71a4e8ccb028726ebce418286c9132282201
BenjiChejtman <46895375@est.ort.edu.ar>
2022-04-06 14:29:19 -0300

    Button working

--- Assets/Scripts/Ejercicio 2/ValidarContrasenia.cs
+++ Assets/Scripts/Ejercicio 2/ValidarContrasenia.cs
@@ -1,13 +1,17 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ValidarContrasenia : MonoBehaviour
 {
-    // Start is called before the first frame update
+    string ContraseniaCorrecta;
+    string ContraseniaUsuario;
+    public Text IngresoUsuario;
+
     void Start()
     {
-        
+        ContraseniaCorrecta = "12345";
     }
 
     // Update is called once per frame
@@ -18,6 +22,14 @@ public class ValidarContrasenia : MonoBehaviour
 
     public void validarContrasenia()
     {
-
+        ContraseniaUsuario = IngresoUsuario.text;
+        if (ContraseniaUsuario == ContraseniaCorrecta)
+        {
+            Debug.Log("Bienvenido");
+        }
+        else
+        {
+            Debug.Log("Contraseña incorrecta");
+        }
     }
 }
