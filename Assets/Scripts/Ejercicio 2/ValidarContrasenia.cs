e75c2bb1522a689fdf8c53c29a358e60101884b8
BenjiChejtman <46895375@est.ort.edu.ar>
2022-04-19 11:35:00 -0300

    Final commit

--- Assets/Scripts/Ejercicio 2/ValidarContrasenia.cs
+++ Assets/Scripts/Ejercicio 2/ValidarContrasenia.cs
@@ -8,10 +8,12 @@ public class ValidarContrasenia : MonoBehaviour
     string ContraseniaCorrecta;
     string ContraseniaUsuario;
     public Text IngresoUsuario;
-
+    public Text textoMsj;
+    public GameObject cartelitoMsj;
     void Start()
     {
         ContraseniaCorrecta = "12345";
+        cartelitoMsj.SetActive(false);
     }
 
     // Update is called once per frame
@@ -25,10 +27,14 @@ public class ValidarContrasenia : MonoBehaviour
         ContraseniaUsuario = IngresoUsuario.text;
         if (ContraseniaUsuario == ContraseniaCorrecta)
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Bienvenido";
             Debug.Log("Bienvenido");
         }
         else
         {
+            cartelitoMsj.SetActive(true);
+            textoMsj.text = "Contraseña inocrrecta";
             Debug.Log("Contraseña incorrecta");
         }
     }
